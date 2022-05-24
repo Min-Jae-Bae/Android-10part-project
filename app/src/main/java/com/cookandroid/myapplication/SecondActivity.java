78d15944b79d3b752440b4abb9f86035666f64f6
Min-Jae <qjrtm1000@gmail.com>
2022-05-24 14:58:39 +0900

    Hello

--- app/src/main/java/com/cookandroid/myapplication/SecondActivity.java
+++ app/src/main/java/com/cookandroid/myapplication/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.cookandroid.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,15 @@ public class SecondActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
+
+        Button btnNewAct = (Button) findViewById(R.id.btnNewAct);
+        btnNewAct.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        ThirdActivity.class);
+                startActivity(intent);
+            }
+                                     });
 
         Button btnReturn = (Button) findViewById(R.id.btnReturn);
         btnReturn.setOnClickListener(new View.OnClickListener() {
